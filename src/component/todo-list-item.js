d6d812f542a3814c160100c46a98386e9a25f73d
genalll <genalll@mail.ru>
2021-07-01 15:18:29 +0500

    Добавил обработчик события по клику.

--- src/component/todo-list-item.js
+++ src/component/todo-list-item.js
@@ -14,7 +14,7 @@ export default class TodoListItem extends React.Component{
             }
         }
         const {lable,important=false}=this.props;
-        return (<span style={styleImportant(important)} className="todolistitem">{lable}
+        return (<span onClick={()=> alert('Привет')} style={styleImportant(important)} className="todolistitem">{lable}
         <div className="button">
         <button className="btn btn-primary" type="submit"><i className="bi bi-alarm alarm"></i></button>
         <button className="btn btn-primary" type="submit"><i className="bi bi-bucket-fill"></i></button>
