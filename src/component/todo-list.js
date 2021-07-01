6874254d6ecf8d59f1b51e4d8528692be661441f
genalll <genalll@mail.ru>
2021-07-01 11:22:41 +0500

    кастомные css

--- src/component/todo-list.js
+++ src/component/todo-list.js
@@ -1,13 +1,14 @@
 import React from "react";
 import TodoListItem from "./todo-list-item"; //Импортируем React
+import "./todo-list.css";
 
 
 
 const ToDoList=({todos})=> {
   const elements=todos.map((item)=>{
-    return <li key={item.id}> <TodoListItem lable={item.lable} important={item.important}/></li>
+    return <li key={item.id} className="list-group-item"> <TodoListItem lable={item.lable} important={item.important}/></li>
   }
   )
-  return <ul>{elements}</ul>
+  return <ul className="list-group todo-list">{elements}</ul>
 };
 export default ToDoList;
