349a6c9df907eed1bb95e976d5e3d2ddcd26885f
genalll <genalll@mail.ru>
2021-07-01 14:19:36 +0500

    переделал отрисовку из func в class

--- src/component/todo-list-item.js
+++ src/component/todo-list-item.js
@@ -2,24 +2,25 @@ import React from "react";
 import "./todo-list-item.css";
 
 
-const styleImportant=(important)=>{
-    if (important){
-         return {color:"red"}
+
+
+
+export default class TodoListItem extends React.Component{
+    
+    render(){
+        const styleImportant=(important)=>{
+            if (important){
+                 return {color:"red"}
+            }
+        }
+        const {lable,important=false}=this.props;
+        return (<span style={styleImportant(important)} className="todolistitem">{lable}
+        <div className="button">
+        <button className="btn btn-primary" type="submit"><i className="bi bi-alarm alarm"></i></button>
+        <button className="btn btn-primary" type="submit"><i className="bi bi-bucket-fill"></i></button>
+        </div>
+        </span>
+
+        )
     }
 }
-const TodoListItem=({lable,important = false})=>{
-   console.log(lable);
-    return (<span style={styleImportant(important)} className="todolistitem">{lable}
-            <div className="button">
-            <button className="btn btn-primary" type="submit"><i className="bi bi-alarm alarm"></i></button>
-            <button className="btn btn-primary" type="submit"><i className="bi bi-bucket-fill"></i></button>
-            </div>
-            </span>
-    
-            )
-    
-       
-    
-}
-
-export default TodoListItem;
