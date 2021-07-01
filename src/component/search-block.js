28c1e7e6f615e6bfe8423c540e5a1b7c9258dda8
genalll <genalll@mail.ru>
2021-07-01 12:55:43 +0500

    Исправил className

--- src/component/search-block.js
+++ src/component/search-block.js
@@ -4,10 +4,10 @@ const SearchBlock=()=>{
     return (
       <div className="centr">
       <input className="search-block" placeholder="Поиск"></input>
-      <div class="btn-group" role="group" aria-label="Basic example">
-      <button type="button" class="btn btn-primary">Все</button>
-      <button type="button" class="btn btn-primary">Активные</button>
-      <button type="button" class="btn btn-primary">Выполненные</button>
+      <div className="btn-group" role="group" aria-label="Basic example">
+      <button type="button" className="btn btn-primary">Все</button>
+      <button type="button" className="btn btn-primary">Активные</button>
+      <button type="button" className="btn btn-primary">Выполненные</button>
       </div>
       </div>
     )
--- src/component/todo-list-item.js
+++ src/component/todo-list-item.js
@@ -11,8 +11,8 @@ const TodoListItem=({lable,important = false})=>{
    console.log(lable);
     return (<span style={styleImportant(important)} className="todolistitem">{lable}
             <div className="button">
-            <button class="btn btn-primary" type="submit"><i class="bi bi-alarm alarm"></i></button>
-            <button class="btn btn-primary" type="submit"><i class="bi bi-bucket-fill"></i></button>
+            <button className="btn btn-primary" type="submit"><i className="bi bi-alarm alarm"></i></button>
+            <button className="btn btn-primary" type="submit"><i className="bi bi-bucket-fill"></i></button>
             </div>
             </span>
     
